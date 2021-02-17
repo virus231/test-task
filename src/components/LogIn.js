6ebc591317eeb636f8161f690572ecfa8d840dd3
Vlad <vladprodan6@gmail.com>
2021-02-17 12:42:21 +0200

    add react-hook-form

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -2,6 +2,10 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
+import { useForm, Controller } from "react-hook-form";
+import {FormControl} from "@material-ui/core";
+import {useState} from "react";
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -9,6 +13,7 @@ const useStyles = makeStyles((theme) => ({
         '& .MuiTextField-root': {
             margin: theme.spacing(1),
             width: '25ch',
+            display: 'block',
         },
         '& h1': {
             textTransform: 'uppercase',
@@ -25,7 +30,6 @@ const useStyles = makeStyles((theme) => ({
                 background: 'linear-gradient(90deg, #FF9146 0%, #FF351B 100%);',
                 border: 0,
                 borderRadius: 5,
-                // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
                 height: 48,
                 padding: '0 60px',
                 textTransform: 'uppercase',
@@ -56,48 +60,84 @@ const useStyles = makeStyles((theme) => ({
 
 export const LogIn = () => {
     const classes = useStyles();
+    const [generalLoginError, setGeneralLoginError] = useState('');
+    const { register,control, handleSubmit, errors: fieldsErrors } = useForm();
     const preventDefault = (event) => event.preventDefault();
 
+    const onSubmitLogin = (data) => {
+        console.log(data);
+    };
+
+    const onErrors = errors => console.error(errors);
 
     return (
-        <div>
-            <form className={classes.root} noValidate autoComplete="off">
-                <h1>LogIn</h1>
-                <div className="form-wrapper">
-                    <TextField
-                        id="outlined-email-input"
-                        label="Email address"
-                        type="text"
-                        autoComplete="current-email"
-                        variant="outlined"
+        <div className={classes.root}>
+            <h1>LogIn</h1>
+            <form className="form-wrapper" onSubmit={handleSubmit(onSubmitLogin)}>
+                <FormControl className={classes.margin} variant="outlined">
+                    <Controller
+                        name="email"
+                        as={
+                            <TextField
+                                id="email"
+                                labelWidth={10}
+                                helperText={fieldsErrors.email ? fieldsErrors.email.message : null}
+                                variant="outlined"
+                                label="Email address"
+                                error={fieldsErrors.email}
+                            />
+                        }
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                            required: 'Email required',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                message: 'invalid email address'
+                            }
+                        }}
                     />
-                    <TextField
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        variant="outlined"
+                </FormControl>
+                <FormControl className={classes.margin} variant="outlined">
+                    <Controller
+                        name="password"
+                        as={
+                            <TextField
+                                id="password"
+                                type="password"
+                                labelWidth={70}
+                                helperText={fieldsErrors.password ? fieldsErrors.password.message : null}
+                                variant="outlined"
+                                label="Password"
+                                error={fieldsErrors.password}
+                            />
+                        }
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                            required: 'Password required'
+                        }}
                     />
-                    <Button>
-                        Log In
-                    </Button>
-                    <div className="form-links">
+                </FormControl>
+                {generalLoginError && <div>{generalLoginError}</div>}
+                <Button type="submit">
+                    Login
+                </Button>
+                <div className="form-links">
+                    <Link href="#" onClick={preventDefault}>
+                        Forgot your password?
+                    </Link>
+                    <div>
                         <Link href="#" onClick={preventDefault}>
-                            Forgot your password?
+                            Don’t have an account yet?
                         </Link>
-                        <div>
-                            <Link href="#" onClick={preventDefault}>
-                                Don’t have an account yet?
-                            </Link>
-                            <Link href="#" onClick={preventDefault}>
-                                Register
-                            </Link>
-                        </div>
+                        <Link href="#" onClick={preventDefault}>
+                            Register
+                        </Link>
                     </div>
-
-
                 </div>
             </form>
+
         </div>
     )
 }
