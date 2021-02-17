10f27ed083e2158f0c48a7be010ed8fc8283f818
Vlad <vladprodan6@gmail.com>
2021-02-17 10:50:54 +0200

    set bg image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import {Paper} from "@material-ui/core";
+import Image from './assets/img/bg-image.png'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: 0,
+    flexGrow: 1,
+    backgroundImage: `url(${Image})`,
+    backgroundSize: 'cover',
+    backgroundPositionY: 'bottom',
+    height: '100vh',
+    backgroundRepeat: 'no-repeat',
+    '& .MuiGrid-container': {
+      height: '100vh'
+    }
+  },
+  paper: {
+    padding: theme.spacing(2),
+    marginRight: theme.spacing(10),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
-  return (
-    <div>
+  const classes = useStyles();
 
-    </div>
+  return (
+      <div className={classes.root}>
+        <Grid
+        container
+        direction="row"
+        justify="flex-end"
+        alignItems="center"
+        height="100vh"
+          >
+            <Grid item xs={12} sm={6} lg={4}>
+                <Paper spacing={10} className={classes.paper}>xs=12 sm=6</Paper>
+            </Grid>
+
+        </Grid>
+      </div>
   );
 }
 
